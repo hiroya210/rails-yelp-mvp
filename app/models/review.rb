60da007bc2e3a1d7cec751321c0644eb6061128f
Hiroya Takemura <hirotakemura210@gmail.com>
2022-02-13 06:46:46 +0900

    yey

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :rating, :content, presence: true
-  validates :rating, inclusion: { in: (0..5) }
   validates :rating, numericality: true
+  validates :rating, inclusion: { in: 0..5 }
 end
